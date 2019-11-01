b86e0a8c9daa636ed322e1a520c578fceeb0fc2e
kerki2000 <56009083+kerki2000@users.noreply.github.com>
2019-11-01 19:03:11 -0400

    hello

---
+++ Employee.java
@@ -0,0 +1,25 @@
+package ArraysLesson;
+
+public class Employee {
+  private String name;
+  private Double salary;
+   Employee(String name, Double salary){
+	   this.name = name;
+	   this.salary = salary;
+   }
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public Double getSalary() {
+	return salary;
+}
+public void setSalary(Double salary) {
+	this.salary = salary;
+}
+   public String toString() {
+	   return "Name "+name+" Salary "+salary;
+   }
+}
